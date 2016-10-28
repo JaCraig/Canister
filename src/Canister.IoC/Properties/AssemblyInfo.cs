939a22f511cfff5ed8417650a3cdc81c15364861
James Craig <none@none>
2016-10-28 15:49:42 -0400

    Renaming the project to make it able to upload to nuget.

--- src/Canister.IoC/Properties/AssemblyInfo.cs
+++ src/Canister.IoC/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Canister")]
+[assembly: AssemblyProduct("Canister.IoC")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -16,4 +16,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("380c4d8c-d0c5-452b-84e0-2d6686236d52")]
+[assembly: Guid("ab6b2461-f558-48f9-82e9-00367d253f43")]
