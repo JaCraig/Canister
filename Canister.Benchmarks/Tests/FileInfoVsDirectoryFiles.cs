e79d6095c3da95d2bcd249bcbd2b2c73e40a8cbd
JaCraig <craig.jamesa@gmail.com>
2024-03-26 17:13:26 -0400

    chore: Adding a benchmark test
    * Testing hash set over list for module discovery

--- Canister.Benchmarks/Tests/FileInfoVsDirectoryFiles.cs
+++ Canister.Benchmarks/Tests/FileInfoVsDirectoryFiles.cs
@@ -4,17 +4,17 @@ using System.IO;
 namespace Canister.Benchmarks.Tests
 {
     [MemoryDiagnoser, AllStatisticsColumn, RankColumn, RPlotExporter]
-    public static class FileInfoVsDirectoryFiles
+    public class FileInfoVsDirectoryFiles
     {
         [Benchmark(Baseline = true)]
-        public static void DirectoryFiles()
+        public void DirectoryFiles()
         {
             foreach (var File in Directory.EnumerateFiles(".", "*.dll"))
             { }
         }
 
         [Benchmark]
-        public static void EnumerateFiles()
+        public void EnumerateFiles()
         {
             foreach (FileInfo File in new DirectoryInfo(".").EnumerateFiles("*.dll"))
             { }
---
+++ Canister.Benchmarks/Tests/FindModulesImprovements.cs
@@ -0,0 +1,110 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Canister.Benchmarks.Tests
+{
+    [MemoryDiagnoser, AllStatisticsColumn, RankColumn, RPlotExporter]
+    public class FindModulesImprovements
+    {
+        [Benchmark]
+        public void HashSetVersion()
+        {
+            _ = FindModulesNew();
+        }
+
+        [Benchmark(Baseline = true)]
+        public void Original()
+        {
+            _ = FindModules();
+        }
+
+        private static Assembly[] FindModules()
+        {
+            var EntryAssembly = Assembly.GetEntryAssembly();
+            if (EntryAssembly is null)
+                return Array.Empty<Assembly>();
+
+            var ExecutingAssembly = Assembly.GetExecutingAssembly();
+            if (ExecutingAssembly is null)
+                return Array.Empty<Assembly>();
+
+            var AssembliesFound = new List<Assembly>
+            {
+                EntryAssembly,
+                ExecutingAssembly
+            };
+            IEnumerable<string> PathsFound = new List<string>
+            {
+                Path.GetDirectoryName(EntryAssembly.Location) ?? "",
+                Path.GetDirectoryName(ExecutingAssembly.Location) ?? ""
+            }.Distinct();
+
+            foreach (var Path in PathsFound)
+            {
+                AssembliesFound.AddRange((IEnumerable<Assembly>)Directory.EnumerateFiles(Path, "*.dll", SearchOption.TopDirectoryOnly)
+                    .Select(assemblyPath =>
+                    {
+                        try
+                        {
+                            return Assembly.LoadFrom(assemblyPath);
+                        }
+                        catch (Exception)
+                        {
+                            return null;
+                        }
+                    })
+                    .Where(assembly => assembly is not null && !AssembliesFound.Contains(assembly)));
+            }
+
+            return AssembliesFound.Distinct().ToArray();
+        }
+
+        private static Assembly[] FindModulesNew()
+        {
+            var EntryAssembly = Assembly.GetEntryAssembly();
+            if (EntryAssembly is null)
+                return Array.Empty<Assembly>();
+
+            var ExecutingAssembly = Assembly.GetExecutingAssembly();
+            if (ExecutingAssembly is null)
+                return Array.Empty<Assembly>();
+
+            var AssembliesFound = new HashSet<Assembly>
+            {
+                EntryAssembly,
+                ExecutingAssembly
+            };
+            var PathsFound = new HashSet<string>
+            {
+                Path.GetDirectoryName(EntryAssembly.Location) ?? "",
+                Path.GetDirectoryName(ExecutingAssembly.Location) ?? ""
+            };
+
+            foreach (var Path in PathsFound)
+            {
+                foreach (var Assembly in Directory.EnumerateFiles(Path, "*.dll", SearchOption.TopDirectoryOnly)
+                    .Select(assemblyPath =>
+                    {
+                        try
+                        {
+                            return Assembly.LoadFrom(assemblyPath);
+                        }
+                        catch (Exception)
+                        {
+                            return null;
+                        }
+                    })
+                    .Where(assembly => assembly is not null))
+                {
+                    AssembliesFound.Add(Assembly);
+                }
+            }
+
+            return AssembliesFound.ToArray();
+        }
+    }
+}
